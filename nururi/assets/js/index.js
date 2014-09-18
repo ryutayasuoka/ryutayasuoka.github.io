5ee09f7f9f677913e4a1d352d5f9179d47c3cfdf
ryuta yasuoka <dragonclimate@gmail.com>
2014-09-18 22:34:27 +0900

    implement parallax, but too heavy...

--- nururi/assets/js/index.js
+++ nururi/assets/js/index.js
@@ -1,16 +1,28 @@
 /*----------------------------------------------------
 
 ----------------------------------------------------*/
-$(function(){
+$(document).ready(function(){
+    init();
+    function init(){
+        $('#member1').css({
+            left: '-100%'
+        });
+    }
+
     $(window).scroll(function(){
+
         var scroll_val = $(document).scrollTop();
-        console.log(scroll_val);
+        //if(scroll_val % 2 == 1)return false;
+        //console.log(scroll_val);
 
         // top
-        $('.logo li').each(function(){
-            var delta = -scroll_val*parseInt($(this).data('y'));
-            $(this).css('top',delta*0.3);
-        });
+        if(scroll_val > -100 && scroll_val < 1000){
+            $('.logo li').each(function(){
+                var delta = -scroll_val*parseInt($(this).data('y'));
+                $(this).css('top',delta*0.3);
+            });
+        }
+
         // top_logo
         if(scroll_val >= 500 && $('#top_logo').data('isshow') == true){
             $('#top_logo').fadeOut(300);
@@ -24,19 +36,47 @@ $(function(){
         if(scroll_val >= 1000 && $('header').data('isshow') == false){
             $('header').fadeIn(500);
             $('header').data('isshow',true);
-            console.log($('header').data('isshow'));
         }else if(scroll_val < 1000 && $('header').data('isshow') == true){
             $('header').fadeOut(500);
             $('header').data('isshow',false);
         }
 
-        //member1
-        if(scroll_val >= 1100){
-            $('#m1').animate({
-                top : '52px'
-            },1000);
+        //member
+        headerAction();
+
+        if(scroll_val >=1000 && scroll_val <=2000){
+            $('#member1').css({
+                left: '-'+((2000-scroll_val)*0.1)+'%',
+             });
+        }else if(scroll_val>2000 && scroll_val<=4000){
+            $('#member1').css({
+                left: '0%',
+                top : scroll_val-30,
+                opacity : (scroll_val-1500)*0.001
+            });
         }
 
+        function headerAction(){
+            if(scroll_val >= 1500 && $('#m1').data('isdown')==false){
+                $('#m1').animate({
+                    top : '52px'
+                },{
+                    duration : 300,
+                    complete : function(){
+                        $('#m1').data('isdown',true);
+                    }
+                });
+            }else if(scroll_val < 1300 && $('#m1').data('isdown')==true){
+                $('#m1').animate({
+                    top : '0px'
+                },{
+                    duration : 300,
+                    complete : function(){
+                        $('#m1').data('isdown',false);
+                    }
+                });
+            }
+        }
     });
 
 });
